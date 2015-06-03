a12973cf51ffa16ac06b60e43b15359d6f8cac45
Mike Overby <mikeoverby@outlook.com>
2015-06-03 02:54:36 -0700

    Create workClock.py

---
+++ workClock.py
@@ -0,0 +1,44 @@
+import time
+import winsound
+
+SECONDS_IN_MIN = 60
+DEFAULT_WORK = 10
+DEFAULT_BREAK = 5
+
+def wait(minutes):
+    
+    print(minutes, "minutes remaining...")
+    
+    timestart = time.time()
+    while (minutes > 0):
+        if time.time() - timestart > SECONDS_IN_MIN:
+            minutes -= 1
+            if minutes > 0:
+                print(minutes, "minutes remaining...")
+            timestart = time.time()
+
+    
+def main():
+    automatic = "q"
+    while automatic != "y" and automatic != "n":
+        automatic = input("Do you want automatic?(y/n): ")
+
+    
+    while(True):
+        if automatic != "y":
+            t = int(input("Work how long? "))
+        else:
+            t = DEFAULT_WORK
+        print("Work time!")
+        wait(t)
+        winsound.Beep(300,2000)
+
+        if automatic != "y":
+            t = int(input("Rest how long? "))
+        else:
+            t = DEFAULT_BREAK
+        print("Break time!")
+        wait(t)
+        winsound.Beep(500,2000)
+    
+main()
