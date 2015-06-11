2162b7c829b495bc449cd2287bb25c116e7d1a9e
Mike Overby <mikeoverby@outlook.com>
2015-06-11 01:37:30 -0700

    Replaced checking the time with sleep
    Just let Python do the work

--- workClock.py
+++ workClock.py
@@ -1,21 +1,16 @@
 import time
 import winsound
 
-SECONDS_IN_MIN = 60
+SECONDS_IN_MIN = 1
 DEFAULT_WORK = 10
 DEFAULT_BREAK = 5
 
+
 def wait(minutes):
-    
-    print(minutes, "minutes remaining...")
-    
-    timestart = time.time()
-    while (minutes > 0):
-        if time.time() - timestart > SECONDS_IN_MIN:
-            minutes -= 1
-            if minutes > 0:
-                print(minutes, "minutes remaining...")
-            timestart = time.time()
+    while minutes > 0:
+        print(minutes, "minutes remaining...")
+        time.sleep(SECONDS_IN_MIN)
+        minutes -= 1
 
     
 def main():
@@ -24,13 +19,13 @@ def main():
         automatic = input("Do you want automatic?(y/n): ")
 
     
-    while(True):
+    while True:
         if automatic == "y":
             t = DEFAULT_WORK
         else:
             t = int(input("Work how long? "))
         print("Work time!")
-        winsound.Beep(300,2000)
+        winsound.Beep(300, 2000)
         wait(t)
 
         if automatic == "y":
@@ -38,8 +33,8 @@ def main():
         else:
             t = int(input("Rest how long? "))
         print("Break time!")
-        winsound.Beep(500,750)
-        winsound.Beep(500,750)
+        winsound.Beep(500, 750)
+        winsound.Beep(500, 750)
         wait(t)
     
 main()
