b722b265299153476865c59024e8179b958e867c
Mike Overby <mikeoverby@outlook.com>
2016-12-30 17:26:50 -0500

    Adding all the JS. Will remove extra stuff later.

---
+++ cherrypy/static/unitconverter.js
@@ -0,0 +1,21 @@
+conversion_map = {
+	"inch cm":1.0/2.54,
+	"cm inch":2.54
+};
+
+$(document).ready(function () {
+	$("button").button({icons: {primary: 'ui-icon-refresh'}}).click(function(){
+			value=$("input[name='from']").val();
+			f=$("select[name='tounit'] option:selected").val();
+			t=$("select[name='fromunit'] option:selected").val();
+			if(f != t){
+				c=conversion_map[f+' '+t];
+				result=parseFloat(value)*c;
+			}else{
+				result = value;
+			}
+			$("input[name='to']").val(result);
+		}
+	);
+});
+$("form *").addClass("ui-widget");
